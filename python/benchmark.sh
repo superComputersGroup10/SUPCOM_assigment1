242c1df054ff6e0b543025226b9375b4a04a060c
“Santiago <santiago.narvaez@tum.de>
2017-11-11 12:56:42 +0100

    Add initial automatic shell script

---
+++ python/benchmark.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+PYTHON_GEN_SCRIPT="comb_generator.py"
+PYTHON_GEN_MAKE="Makefiles_generator.py"
+COMPILER="gcc"
+FLAGS_FILE="flags_combination"
+
+echo "Starting..."
+
+
+chmod +x $PYTHON_GEN_SCRIPT $PYTHON_GEN_MAKE
+./$PYTHON_GEN_SCRIPT -c $COMPILER -o $FLAGS_FILE
+
+#for i in {1..127};
+#do
+#make -f Makefile$i 
+#mv lulesh2.0_$i Binaries/
+#make -f Makefile$i clean
