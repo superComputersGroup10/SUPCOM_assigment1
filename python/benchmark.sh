45b458fd70e7740902ff173988f04dcd645056cb
“Santiago <santiago.narvaez@tum.de>
2017-11-11 13:56:34 +0100

    Set number of combinations depending on compiler

--- python/benchmark.sh
+++ python/benchmark.sh
@@ -5,13 +5,22 @@ PYTHON_GEN_MAKE="Makefiles_generator.py"
 COMPILER="gcc"
 FLAGS_FILE="flags_combination"
 
-echo "Starting..."
+if [ "$COMPILER" = "gcc" ]; then
+    NUM_COMBINATIONS=127
+elif [ "$COMPILER" = "icc" ]; then
+    NUM_COMBINATIONS=15
+else
+    (>&2 echo "ERROR: Unknown compiler")
+    exit
+fi
 
+echo "Starting..."
+echo "$NUM_COMBINATIONS"
 
 python $PYTHON_GEN_SCRIPT -c $COMPILER -o $FLAGS_FILE
 
 #for i in {1..127};
 #do
-#make -f Makefile$i 
+#make -f Makefile$i
 #mv lulesh2.0_$i Binaries/
 #make -f Makefile$i clean
