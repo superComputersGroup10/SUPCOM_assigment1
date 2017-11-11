1572582e554a2aceb1c203387bc82fc6a79a734e
“Santiago <santiago.narvaez@tum.de>
2017-11-11 14:23:51 +0100

    Create directories for output

--- python/benchmark.sh
+++ python/benchmark.sh
@@ -4,6 +4,11 @@ PYTHON_GEN_SCRIPT="comb_generator.py"
 PYTHON_GEN_MAKE="Makefiles_generator.py"
 COMPILER="gcc"
 FLAGS_FILE="flags_combination"
+SUPERMUC_PHASE=1
+SCRIPT_DIRECTORY="$(pwd) "
+CODE_DIRECTORY="$(dirname $(pwd))"
+OUTPUT_DIRECTORY="$CODE_DIRECTORY/4_2_compile_flags/phase$SUPERMUC_PHASE/$COMPILER"
+
 
 if [ "$COMPILER" = "gcc" ]; then
     NUM_COMBINATIONS=127
@@ -15,9 +20,9 @@ else
 fi
 
 echo "Starting..."
-echo "$NUM_COMBINATIONS"
 
 python $PYTHON_GEN_SCRIPT -c $COMPILER -o $FLAGS_FILE
+mkdir -p -v $OUTPUT_DIRECTORY
 
 #for i in {1..127};
 #do
