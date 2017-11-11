b72522a3707815dfdc52327c1a28c63853e20327
“Santiago <santiago.narvaez@tum.de>
2017-11-11 15:31:55 +0100

    Compile, run and save data

--- python/benchmark.sh
+++ python/benchmark.sh
@@ -1,16 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
 PYTHON_GEN_SCRIPT="comb_generator.py"
 PYTHON_GEN_MAKE="Makefiles_generator.py"
-COMPILER="gcc"
+COMPILER="g++"
 FLAGS_FILE="flags_combination"
 SUPERMUC_PHASE=1
+
 SCRIPT_DIRECTORY="$(pwd) "
 CODE_DIRECTORY="$(dirname $(pwd))"
-OUTPUT_DIRECTORY="$CODE_DIRECTORY/4_2_compile_flags/phase$SUPERMUC_PHASE/$COMPILER"
+MAKE_FILE="$CODE_DIRECTORY/MakefileBenchmark"
+BINARY="$CODE_DIRECTORY/lulesh2.0"
+OUTPUT_DIRECTORY="$CODE_DIRECTORY/4_2_compile_flags/phase$SUPERMUC_PHASE/$COMPILER/"
+OUTPUT_FILES_SUFIX="output_"
 
-
-if [ "$COMPILER" = "gcc" ]; then
+if [ "$COMPILER" = "g++" ]; then
     NUM_COMBINATIONS=127
 elif [ "$COMPILER" = "icc" ]; then
     NUM_COMBINATIONS=15
@@ -24,8 +27,11 @@ echo "Starting..."
 python $PYTHON_GEN_SCRIPT -c $COMPILER -o $FLAGS_FILE
 mkdir -p -v $OUTPUT_DIRECTORY
 
-#for i in {1..127};
-#do
-#make -f Makefile$i
-#mv lulesh2.0_$i Binaries/
-#make -f Makefile$i clean
+make --directory=$CODE_DIRECTORY --file=$MAKE_FILE clean
+for (( c=0; c<$NUM_COMBINATIONS; c++  ))
+do
+    python $PYTHON_GEN_MAKE -i $FLAGS_FILE -r $c -c $COMPILER -o $MAKE_FILE
+    make --directory=$CODE_DIRECTORY --file=$MAKE_FILE
+    $BINARY > $OUTPUT_DIRECTORY$OUTPUT_FILES_SUFIX$((c+1))
+    make --directory=$CODE_DIRECTORY --file=$MAKE_FILE clean
+done
