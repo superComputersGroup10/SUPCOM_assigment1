a9c314b32980830425854e85a5b01be265f11118
“Santiago <santiago.narvaez@tum.de>
2017-11-11 13:09:39 +0100

    Not make python scripts executable
    It is not necessary to make the python scripts executable because we
    can run them simply by using: python [python-script]

--- python/benchmark.sh
+++ python/benchmark.sh
@@ -8,13 +8,10 @@ FLAGS_FILE="flags_combination"
 echo "Starting..."
 
 
-chmod +x $PYTHON_GEN_SCRIPT $PYTHON_GEN_MAKE
-./$PYTHON_GEN_SCRIPT -c $COMPILER -o $FLAGS_FILE
+python $PYTHON_GEN_SCRIPT -c $COMPILER -o $FLAGS_FILE
 
 #for i in {1..127};
 #do
 #make -f Makefile$i 
 #mv lulesh2.0_$i Binaries/
 #make -f Makefile$i clean
-
-chmod -x $PYTHON_GEN_SCRIPT $PYTHON_GEN_MAKE
