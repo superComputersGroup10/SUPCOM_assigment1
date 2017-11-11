62fe5a44a9be343167f62c2252c289a38cae17e8
“Santiago <santiago.narvaez@tum.de>
2017-11-11 12:42:14 +0100

    Add flags to select output and compiler

--- python/comb_generator.py
+++ python/comb_generator.py
@@ -1,19 +1,36 @@
 #!/usr/bin/python3
 
 import itertools
+import argparse
+
+def gen_combinations(flags, output_file):
+    with open(output_file, "w") as f:
+        for c in range(1, len(flags) + 1):
+            for c_flag in list(itertools.combinations(flags, c)):
+                f.write(' '.join(c_flag) + "\n")
 
 
 def main():
-    flags = ["-march=native", "-fomit-frame-pointer", "-floop-block", "-floop-interchange",
-            "-floop-strip-mine", "-funroll-loops", "-flto"]
+    parser = argparse.ArgumentParser(description="Generates combination of compiling flags")
+    parser.add_argument("-o", "--output",
+            help="Output file",
+            default="flags_combination")
+    parser.add_argument("-c", "--compiler",
+            help="Compiler to use",
+            default="gcc")
+    args = parser.parse_args()
+
+    if args.compiler == "gcc":
+        flags = ["-march=native", "-fomit-frame-pointer", "-floop-block", "-floop-interchange",
+                "-floop-strip-mine", "-funroll-loops", "-flto"]
+    else:
+        flags = ["-march=native", "-xHost", "-unroll", "-ipo"]
 
     print("Generating combination of following flags:")
     print(" ".join(flags))
 
-    with open("flags_combination", "w") as f:
-        for c in range(1, len(flags) + 1):
-            for c_flag in list(itertools.combinations(flags, c)):
-                f.write(' '.join(c_flag) + "\n")
+    gen_combinations(flags, args.output)
+
 
 if __name__ == "__main__":
     main()
