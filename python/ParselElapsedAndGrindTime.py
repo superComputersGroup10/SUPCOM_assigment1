4cabb53bfc99068d46ec2c4d539acebe8694ab69
Anibal <anibal.siguenza1@gmail.com>
2017-11-13 16:35:21 +0100

    Computing speed up

--- python/ParselElapsedAndGrindTime.py
+++ python/ParselElapsedAndGrindTime.py
@@ -27,26 +27,35 @@ def main():
     parser.add_argument("-r", "--root_name", help="Root name from output files")
     parser.add_argument("-d", "--directory", help="Directory where the output files are")
     parser.add_argument("-f", "--flags_file", help="File with the list of flags")
+    parser.add_argument("-b", "--baseline_data", help="File with the baseline run to compute the speed-up")
     parser.add_argument("-o", "--ouptut_cvs", help="Name of cvs output")
     args = parser.parse_args()
 
     root_name = args.root_name
     directory = args.directory   
     flags_file = args.flags_file
+    baselineFile = args.baseline_data
     ouptut_cvs = args.ouptut_cvs
 
     fp=open(flags_file) 
     flags=fp.readlines()
 
+    baseElapsed_time, baseGrind_time = readElapsedAndGrindTime(baselineFile)
+
     index=[None] * len(flags)
     Elapsed_time=[None] * len(flags)
     Grind_time=[None] * len(flags)
+
+    Elapsed_time_speed_up=[None] * len(flags)
+    Grind_time_speed_up=[None] * len(flags)
 
     for i in range(0, len(flags)):
         index[i]=i+1
         file_name = directory + '/' + root_name + str(i+1)
         if os.path.isfile(file_name):
             Elapsed_time[i], Grind_time[i] = readElapsedAndGrindTime(file_name)
+            Elapsed_time_speed_up [i] = baseElapsed_time / Elapsed_time[i]
+            Grind_time_speed_up [i] = baseGrind_time / Grind_time[i]
         else:
             Elapsed_time[i]='The file was not found'
             Grind_time[i]='The file was not found'
@@ -58,6 +67,8 @@ def main():
         spamwriter.writerow(flags)
         spamwriter.writerow(Elapsed_time)
         spamwriter.writerow(Grind_time)
+        spamwriter.writerow(Elapsed_time_speed_up)
+        spamwriter.writerow(Grind_time_speed_up)
 
 if __name__ == "__main__":
         main()
