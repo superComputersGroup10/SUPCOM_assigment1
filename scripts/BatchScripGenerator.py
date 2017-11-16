7f9bf13f874b352184d55ff0f0ba3afa3d1b6712
Anibal <anibal.siguenza1@gmail.com>
2017-11-16 16:22:21 +0100

    Add BatchScripGenerato

---
+++ scripts/BatchScripGenerator.py
@@ -0,0 +1,43 @@
+import argparse
+
+def writeBashFile(numThreads, output):
+        bashString="""#!/bin/bash
+#@ wall_clock_limit = 00:20:00
+#@ job_name = pos-lulesh-openmp
+#@ job_type = MPICH
+# #####################################
+# for fat node we need class = micro
+# #@ class = micro
+# #####################################
+# // for thin node we need class = fat
+#@ class = fat
+# ######################################@ output = pos_lulesh_openmp_$(jobid).out
+#@ error = pos_lulesh_openmp_$(jobid).out
+#@ node = 1
+#@ total_tasks = %i
+#@ node_usage = not_shared
+#@ energy_policy_tag = lulesh
+#@ minimize_time_to_solution = yes
+#@ island_count = 1
+#@ queue
+. /etc/profile
+. /etc/profile.d/modules.sh
+export OMP_NUM_THREADS=%i
+./lulesh2.0""" % (numThreads, numThreads)
+        f = open(output, 'w')
+        f.write(bashString)
+        f.closed
+ 
+def main():
+    parser = argparse.ArgumentParser(description="Generates a Batchspript for a jon with n threads")
+    parser.add_argument("-n", "--numThreads", help="Number of threads to run")
+    parser.add_argument("-o", "--output", help="Output name for the Makefile")
+    args = parser.parse_args()
+
+    numThreads = int(args.numThreads )
+    output = args.output 
+
+    writeBashFile(numThreads, output)
+
+if __name__ == "__main__":
+        main()
