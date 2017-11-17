efcb615aafd50a62a255c7f7d9e8abe8c1c7bc7b
Anibal <anibal.siguenza1@gmail.com>
2017-11-17 13:45:09 +0100

    Add an automatic mode for the parsel
    Now if you pass as argument auto, then it does not need a flag files it just search for the file in the indicated folder and save them with the file name

--- scripts/ParselElapsedAndGrindTime.py
+++ scripts/ParselElapsedAndGrindTime.py
@@ -7,6 +7,7 @@ from os.path import isfile, join
 import os
 
 def readElapsedAndGrindTime(file_name):
+    print(file_name)
     f=open(file_name)
     fileStr=f.read().replace('(us/z/c)', '')
     f.close()
@@ -26,7 +27,7 @@ def main():
     parser = argparse.ArgumentParser(description="Generates a .csv with the Elapsed and Grind time")
     parser.add_argument("-r", "--root_name", help="Root name from output files")
     parser.add_argument("-d", "--directory", help="Directory where the output files are")
-    parser.add_argument("-f", "--flags_file", help="File with the list of flags")
+    parser.add_argument("-f", "--flags_file", help="File with the list of flags or auto to intex from 1 to N files found")
     parser.add_argument("-b", "--baseline_data", help="File with the baseline run to compute the speed-up")
     parser.add_argument("-o", "--ouptut_cvs", help="Name of cvs output")
     args = parser.parse_args()
@@ -37,39 +38,75 @@ def main():
     baselineFile = args.baseline_data
     ouptut_cvs = args.ouptut_cvs
 
-    fp=open(flags_file) 
-    flags=fp.readlines()
+    if (flags_file == "auto"):
+        onlyfiles = [f for f in listdir(directory) if isfile(join(directory, f))]
 
-    baseElapsed_time, baseGrind_time = readElapsedAndGrindTime(baselineFile)
+        baseElapsed_time, baseGrind_time = readElapsedAndGrindTime(baselineFile)
 
-    index=[None] * len(flags)
-    Elapsed_time=[None] * len(flags)
-    Grind_time=[None] * len(flags)
+        index=[None] * len(onlyfiles )
+        Elapsed_time=[None] * len(onlyfiles)
+        Grind_time=[None] * len(onlyfiles)
 
-    Elapsed_time_speed_up=[None] * len(flags)
-    Grind_time_speed_up=[None] * len(flags)
+        Elapsed_time_speed_up=[None] * len(onlyfiles)
+        Grind_time_speed_up=[None] * len(onlyfiles)
 
-    for i in range(0, len(flags)):
-        index[i]=i+1
-        file_name = directory + '/' + root_name + str(i+1)
-        if os.path.isfile(file_name):
-            Elapsed_time[i], Grind_time[i] = readElapsedAndGrindTime(file_name)
-            if (isinstance(Elapsed_time[i], float) and isinstance(Grind_time[i], float)):
-                Elapsed_time_speed_up [i] = baseElapsed_time / Elapsed_time[i]
-                Grind_time_speed_up [i] = baseGrind_time / Grind_time[i]
-        else:
-            Elapsed_time[i]='The file was not found'
-            Grind_time[i]='The file was not found'
+        for i in range(0, len(onlyfiles)):
+            index[i]=i+1
+            file_name = directory + '/' + root_name + str(i+1)
+            print(file_name)
+            if os.path.isfile(file_name):
+                Elapsed_time[i], Grind_time[i] = readElapsedAndGrindTime(file_name)
+                if (isinstance(Elapsed_time[i], float) and isinstance(Grind_time[i], float)):
+                    Elapsed_time_speed_up [i] = baseElapsed_time / Elapsed_time[i]
+                    Grind_time_speed_up [i] = baseGrind_time / Grind_time[i]
+            else:
+                Elapsed_time[i]='The file was not found'
+                Grind_time[i]='The file was not found'
 
-    import csv
-    with open(ouptut_cvs, 'w') as csvfile:
-        spamwriter = csv.writer(csvfile, delimiter=',')
-        spamwriter.writerow(index)
-        spamwriter.writerow(flags)
-        spamwriter.writerow(Elapsed_time)
-        spamwriter.writerow(Grind_time)
-        spamwriter.writerow(Elapsed_time_speed_up)
-        spamwriter.writerow(Grind_time_speed_up)
+        import csv
+        with open(ouptut_cvs, 'w') as csvfile:
+            spamwriter = csv.writer(csvfile, delimiter=',')
+            spamwriter.writerow(index)
+            spamwriter.writerow(onlyfiles)
+            spamwriter.writerow(Elapsed_time)
+            spamwriter.writerow(Grind_time)
+            spamwriter.writerow(Elapsed_time_speed_up)
+            spamwriter.writerow(Grind_time_speed_up)
+
+    else:
+        fp=open(flags_file) 
+        flags=fp.readlines()
+
+        baseElapsed_time, baseGrind_time = readElapsedAndGrindTime(baselineFile)
+
+        index=[None] * len(flags)
+        Elapsed_time=[None] * len(flags)
+        Grind_time=[None] * len(flags)
+
+        Elapsed_time_speed_up=[None] * len(flags)
+        Grind_time_speed_up=[None] * len(flags)
+
+        for i in range(0, len(flags)):
+            index[i]=i+1
+            file_name = directory + '/' + root_name + str(i+1)
+            if os.path.isfile(file_name):
+                Elapsed_time[i], Grind_time[i] = readElapsedAndGrindTime(file_name)
+                if (isinstance(Elapsed_time[i], float) and isinstance(Grind_time[i], float)):
+                    Elapsed_time_speed_up [i] = baseElapsed_time / Elapsed_time[i]
+                    Grind_time_speed_up [i] = baseGrind_time / Grind_time[i]
+            else:
+                Elapsed_time[i]='The file was not found'
+                Grind_time[i]='The file was not found'
+
+        import csv
+        with open(ouptut_cvs, 'w') as csvfile:
+            spamwriter = csv.writer(csvfile, delimiter=',')
+            spamwriter.writerow(index)
+            spamwriter.writerow(flags)
+            spamwriter.writerow(Elapsed_time)
+            spamwriter.writerow(Grind_time)
+            spamwriter.writerow(Elapsed_time_speed_up)
+            spamwriter.writerow(Grind_time_speed_up)
 
 if __name__ == "__main__":
         main()
