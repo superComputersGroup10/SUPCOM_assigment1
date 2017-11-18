35b4ac0fb0fb720b7c51b88a3a8ac70a1b6fde54
Anibal <anibal.siguenza1@gmail.com>
2017-11-18 14:00:43 +0100

    Change to only prive 1 number of process with 1 to N threads

--- scripts/HybridBenchmark.sh
+++ scripts/HybridBenchmark.sh
@@ -6,7 +6,7 @@ MODE="supermuc"
 #MODE = "AnibalSuperFunComputer"
 
 COMPILER="mpiCC" # Two possible values, g++ or icc
-MAX_TWO_POWER_PROCESS=3
+TWO_POWER_PROCESS=3
 MAX_NUM_TASKS=4
 SUPERMUC_PHASE=1
 
@@ -36,20 +36,18 @@ make --directory=$CODE_DIRECTORY clean
 make --directory=$CODE_DIRECTORY --file=$MAKE_FILE
 
 # Since the first line of the comb_flags is a blank line, we have to loop over $NUM_COMBINATIONS + 1
-for (( c=1; c<=$MAX_TWO_POWER_PROCESS; c++  ))
-do
-    for (( n=1; n<=$MAX_NUM_TASKS; n++ ))
-    do 
-        echo ""
-        echo "*****************************************************"
-        echo "Running with $c*$c*$c processes and $n threads."
-        python $PYTHON_GEN_BATCH -n $n -c $c -o $BATCH_FILE -l $OUTPUT_DIRECTORY/$OUTPUT_FILES_SUFIX$c.$n
 
-        if [ "$MODE" = "supermuc" ]; then
-            llsubmit $BATCH_FILE
-        else
-            export OMP_NUM_THREADS=c
-            $BINARY > $OUTPUT_DIRECTORY/$OUTPUT_FILES_SUFIX$c.$n
-        fi
-    done
+for (( n=1; n<=$MAX_NUM_TASKS; n++ ))
+do 
+    echo ""
+    echo "*****************************************************"
+    echo "Running with $TWO_POWER_PROCESS*$TWO_POWER_PROCESS*$TWO_POWER_PROCESS processes and $n threads."
+    python $PYTHON_GEN_BATCH -n $n -c $TWO_POWER_PROCESS -o $BATCH_FILE -l $OUTPUT_DIRECTORY/$OUTPUT_FILES_SUFIX$TWO_POWER_PROCESS.$n
+
+    if [ "$MODE" = "supermuc" ]; then
+        llsubmit $BATCH_FILE
+    else
+        export OMP_NUM_THREADS=c
+        $BINARY > $OUTPUT_DIRECTORY/$OUTPUT_FILES_SUFIX$c.$n
+    fi
 done
